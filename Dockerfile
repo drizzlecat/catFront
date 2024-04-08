7a193af43a3dbee3bb94efce59b8d9f38ab5f24d
liufangjun <liu_fangjun@cyberowl.co.jp>
2024-04-09 00:00:00 +0900

    [fix]fix the env

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,11 @@ WORKDIR /app
 COPY package*.json ./
 
 # Step 4: Install dependencies
-RUN npm install
+# RUN npm install
+# RUN npm install webpack webpack-cli --save-dev
 
 # Step 5: Copy the rest of your app's source code
-COPY . .
+# COPY . .
 
 # Step 6: Expose port 3000 to have it mapped by Docker daemon
 EXPOSE 3000
--- aliash.sh
+++ aliash.sh
@@ -3,4 +3,6 @@ docker build -t my-react-app .
 docker run -p 3007:3000 my-react-app
 docker-compose build
 docker-compose run react create-react-app my-react-app .
-docker-compose up --build
+alias lcl="docker-compose up --build"
+// "start": "webpack serve --mode development --open"
+alias lcl-bash="docker exec -it react bash"
